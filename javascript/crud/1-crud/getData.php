5d1da57a6eb8c3e4c69c3bb64d640313966b366a
Nawcodes <nawcodes@gmail.com>
2021-04-07 15:18:16 +0700

    get data crud with ajax on js CRUD

--- javascript/crud/1-crud/getData.php
+++ javascript/crud/1-crud/getData.php
@@ -9,7 +9,7 @@
         $result[] = $fetch;
     }
 
-    var_dump(json_encode($result));
+    echo json_encode($result);
 
 
 ?>
--- javascript/crud/1-crud/js/script.js
+++ javascript/crud/1-crud/js/script.js
@@ -1,10 +1,20 @@
 $.ajax(
     {
         type: "GET",
-        data: '',
+        data: "",
         url : 'getData.php',
         success: function (result) {
-            console.log(result);
+            var objResult = JSON.parse(result);
+            $.each(objResult, function(key,val) {
+                const newLine = $("<tr>");
+                newLine.html(`<td>${val.id}</td>
+                            <td>${val.name}</td>
+                            <td>${val.nim}</td>
+                            <td>${val.number}</td>`
+                            );
+                var dataHandler = $("#data");
+                dataHandler.append(newLine);
+            });
         }
     }
 );
