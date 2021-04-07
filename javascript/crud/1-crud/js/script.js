00242879d2b6244878812bc27387767b2c4ba8f3
Nawcodes <nawcodes@gmail.com>
2021-04-07 15:31:14 +0700

    modified id be number increment jsCRUD

--- javascript/crud/1-crud/js/script.js
+++ javascript/crud/1-crud/js/script.js
@@ -4,16 +4,22 @@ $.ajax(
         data: "",
         url : 'getData.php',
         success: function (result) {
+            // console.log(result); result string
             var objResult = JSON.parse(result);
+            // console.log(objResult); parse array of object
+            // loop
+            let no = 1;
             $.each(objResult, function(key,val) {
                 const newLine = $("<tr>");
-                newLine.html(`<td>${val.id}</td>
+                
+                newLine.html(`<td>${no}</td>
                             <td>${val.name}</td>
                             <td>${val.nim}</td>
                             <td>${val.number}</td>`
                             );
                 var dataHandler = $("#data");
                 dataHandler.append(newLine);
+                no++;
             });
         }
     }
