c35a4ec81bc3c67e039873ea92ec28b44974d786
nawcodes <nawcodes@gmail.com>
2021-04-15 02:00:01 +0700

    modified JS CRUD and add DB_default

--- javascript/crud/db/js-crud.sql
+++ javascript/crud/db/js-crud.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2021 at 09:03 AM
+-- Generation Time: Apr 14, 2021 at 08:52 PM
 -- Server version: 10.4.18-MariaDB
--- PHP Version: 7.3.27
+-- PHP Version: 7.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,8 +31,38 @@ CREATE TABLE `personal` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `nim` varchar(255) NOT NULL,
-  `number` varchar(255) NOT NULL
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `about` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `personal`
+--
+
+INSERT INTO `personal` (`id`, `name`, `nim`, `email`, `phone`, `image`, `about`) VALUES
+(2, 'rifalnurchya', '20180050049', 'rifalnurchya@gmail.com', '085940775599', 'example.jpg', 'Hallo saya adalah rifal');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `personal`
+--
+ALTER TABLE `personal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `personal`
+--
+ALTER TABLE `personal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
