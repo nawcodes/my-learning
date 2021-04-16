2a70cf4c77509413a14caab70ae6189ac0a27320
Nawcodes <nawcodes@gmail.com>
2021-04-17 02:24:52 +0700

    JSADVANCE REST PIZZA add content fillter with menu

--- javascript/rest/1-rest-pizza/js/script.js
+++ javascript/rest/1-rest-pizza/js/script.js
@@ -17,7 +17,38 @@ $.getJSON('data/pizza.json', function(result) {
         </div>`
         );
     });
+});
+
+
+// when LI clicked
+$('.nav-link').on('click', function () {
+    $('.nav-link').removeClass('active'); 
+    $(this).addClass('active');
+    
+    let category = $(this).html();
+    $('h1').html(category);
+    
+    $.getJSON('data/pizza.json', function(result) {
+        let menu = result.menu;
+        let content = '';
+
+        $.each(menu, function (i, val) {
+            if(val.kategori == category.toLowerCase()) {
+                content += `<div class="col-md-4">
+                <div class="card mb-3">
+                    <img src="img/menu/${val.gambar}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${val.nama}</h5>
+                        <p class="card-text">${val.deskripsi}</p>
+                        <h5>Rp. ${val.harga},-</h5>
+                        <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+                    </div>
+                </div>
+            </div>`;
+            $('#menu-list').html(content);
+            }
+        })
+    });
 
 
 });
-
