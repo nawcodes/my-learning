699c48a411633ad025fcfbe9afffc6f96d7e0b22
Nawcodes <nawcodes@gmail.com>
2021-05-10 03:23:57 +0700

    JS ADVANCES DOM 01-DOMSELECTION SUCCESS

---
+++ javascript/dom/1-intro/1.dom-selection/script.js
@@ -0,0 +1,21 @@
+// 1. getElementById
+
+const judul = document.getElementById('judul');
+console.log(judul);
+judul.style.color = 'green';
+judul.innerHTML = 'Hello Rifal';
+
+// 2. getElementsByTagName
+const p = document.getElementsByTagName('p');
+console.log(p); // return array html collection
+for (let i = 0; i < p.length; i++) {
+    const element = p[i];
+    element.style.backgroundColor = 'lightblue';
+}
+const h1 = document.getElementsByTagName('h1')[0];
+console.log(h1);
+
+
+// 3. getElementByClassName
+const p1 = document.getElementsByClassName('p1');
+console.log(p1); // return htmlCollection an array
