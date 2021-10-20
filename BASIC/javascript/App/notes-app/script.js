b4680bb0d3516cb04ffb4ff0e56f61ea61298946
Nawcodes <nawcodes@gmail.com>
2021-10-20 20:26:01 +0700

    add app notes | make structure more compatible

--- BASIC/javascript/App/notes-app/script.js
+++ BASIC/javascript/App/notes-app/script.js
@@ -1,17 +1,79 @@
-const notesEl = document.querySelector('.notes');
-const editBtn = document.querySelector('.edit');
-const deleteBtn = document.querySelector('.delete');
-const main = notesEl.querySelector('.main');
-const textArea = notesEl.querySelector('textarea');
+const addBtn = document.getElementById('add');
 
+const notes = JSON.parse(localStorage.getItem('notes'));
 
-editBtn.addEventListener('click', () => {
-    main.classList.toggle('hidden');    
-    textArea.classList.toggle('hidden');    
+if(notes) {
+    notes.forEach(note => {
+        addNewNote(note);
+    });
+}
+
+addBtn.addEventListener('click', () => {
+    addNewNote();
 })
 
+function addNewNote(text = '') {
+    const note = document.createElement('div');
+    note.classList.add('note');
 
-textArea.addEventListener('input', (e) => {
-    const { value }  = e.target;
-    main.innerHTML = marked(value);
-})
+
+    note.innerHTML = `
+    <div class="notes">
+        <div class="tools">
+            <button class="edit"><i class="fas fa-edit"></i></button>
+            <button class="delete"><i class="fas fa-trash-alt"></i></button>
+        </div>
+        <div class="main ${text ? '' : 'hidden'}">
+        </div>
+        <textarea name="" id=""  class="${text ? 'hidden' : ''}"></textarea>
+    </div> 
+    `;
+
+
+    const editBtn = note.querySelector('.edit');
+    const deleteBtn = note.querySelector('.delete');
+    const main = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+
+
+    textArea.value = text;
+    main.innerHTML = marked(text);
+
+    editBtn.addEventListener('click', () => {
+        main.classList.toggle('hidden');    
+        textArea.classList.toggle('hidden');    
+    })
+
+
+    deleteBtn.addEventListener('click', () => {
+        note.remove();
+        updateLS();
+    });
+
+    textArea.addEventListener('input', (e) => {
+        const { value }  = e.target;
+        main.innerHTML = marked(value);
+
+        // update && save notes when close browser or reload
+        updateLS(); 
+    })
+
+    document.body.appendChild(note);
+
+}
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea');
+
+    const notes = [];
+
+    notesText.forEach(note => {
+        notes.push(note.value);
+    });
+
+    localStorage.setItem('notes', JSON.stringify(notes));
+
+}
+
+
+
