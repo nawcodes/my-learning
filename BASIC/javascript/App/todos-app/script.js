07ae2140f69540b7f7b6162463550f6e53627746
Nawcodes <nawcodes@gmail.com>
2021-10-29 21:11:15 +0700

    modified todos

--- BASIC/javascript/App/todos-app/script.js
+++ BASIC/javascript/App/todos-app/script.js
@@ -1,30 +1,70 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
-const todos = document.getElementById('todos');
+const todosUL = document.getElementById('todos');
+
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if(todos) {
+    todos.forEach(todo => {
+        addTodo(todo);
+    })
+}
 
 form.addEventListener('submit', (e) => {
     // have not ever reload
     e.preventDefault();
+    addTodo('');
+});
 
-    const todoText = input.value;
+function addTodo(todo) {
+   let todoText = input.value;
+
+   if(todo) {
+     todoText = todo.text;
+   }
+
+
 
     if(todoText) {
         const todoEl = document.createElement('li');
-        todoEl.innerText = todoText;
-        todos.appendChild(todoEl)
+        if(todo.completed) {
+            todoEl.classList.add('completed');
+        } 
 
+        todoEl.innerText = todoText;
+        todosUL.appendChild(todoEl)
+        
         todoEl.addEventListener('click', () => {
             todoEl.classList.toggle('completed');
+            updateLS();
         });
 
 
         // when right click is clicked | remove!
-        todoEl.addEventListener('contextmenu', () => {
+        todoEl.addEventListener('contextmenu', (e) => {
             e.preventDefault();
             todoEl.remove();
+            updateLS();
         })
 
         // empty the value input after added
         input.value = '';
+
+        updateLS();
     }
-})
+}
+
+function updateLS() {
+    const todosEl = document.querySelectorAll('li');
+    const todos = [];
+
+    todosEl.forEach(todoEl => {
+        console.log(todoEl);
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        });
+    });
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
