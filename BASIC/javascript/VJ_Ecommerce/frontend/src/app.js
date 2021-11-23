a41a2234e4e685538ebbd0b6b3536d19858522ae
Nawcodes <nawcodes@gmail.com>
2021-11-23 21:45:47 +0700

    add new view ProductScreen , Error 404Screen | modified appjs for routes | add new file utils for routes parseURL

--- BASIC/javascript/VJ_Ecommerce/frontend/src/app.js
+++ BASIC/javascript/VJ_Ecommerce/frontend/src/app.js
@@ -1,9 +1,31 @@
 import homeScreen from './srcreens/HomeScreen.js';
+import ProductScreen from './srcreens/ProductScreen.js';
+import Error404Screen from './srcreens/Error404Screen.js';
+import { parseRequestUrl } from './utils.js';
 
+
+
+
+const routes = {
+    '/': homeScreen,
+    '/product/:id' : ProductScreen,
+}
 
 const router = () => {
+    const request = parseRequestUrl();
     const main = document.getElementById('main-container');
-    main.innerHTML = homeScreen.render();
+
+    const parseUrl = 
+        (request.resource ? `/${request.resource}` : '/') +
+        (request.id ? `/:id` : '') + 
+        (request.verb ? `/${request.verb}` : '');
+
+    const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
+    console.log(request);
+    console.log(parseUrl);
+    console.log(screen);
+    main.innerHTML = screen.render();
 }
 
 window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
---
+++ BASIC/javascript/VJ_Ecommerce/frontend/src/srcreens/Error404Screen.js
@@ -0,0 +1,9 @@
+const Error404Screen = {
+    render: () => { 
+        return `
+        <div>Page Not Found!</div>
+        `;
+    }
+}
+
+export default Error404Screen;
