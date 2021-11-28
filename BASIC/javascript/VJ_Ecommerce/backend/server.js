684e596b34cf922e5915d681e265ed46fab658ae
Nawcodes <nawcodes@gmail.com>
2021-11-29 00:34:05 +0700

    make request to api/products/:byid

--- BASIC/javascript/VJ_Ecommerce/backend/server.js
+++ BASIC/javascript/VJ_Ecommerce/backend/server.js
@@ -10,6 +10,22 @@ app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
 
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => {
+        return x._id === req.params.id;
+    });
+
+    if(product) {
+        res.send(product);
+    }else {
+        res.status(404).send(
+            {
+                message: 'Product not found'
+            }
+        )
+    }
+});
+
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
 });
