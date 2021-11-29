bf002e7c868ad78d12b14794a10d91eb3894b35a
Nawcodes <nawcodes@gmail.com>
2021-11-29 23:05:21 +0700

    give product details style page

--- BASIC/javascript/VJ_Ecommerce/frontend/src/srcreens/ProductScreen.js
+++ BASIC/javascript/VJ_Ecommerce/frontend/src/srcreens/ProductScreen.js
@@ -1,4 +1,5 @@
 import { getProduct } from '../api';
+import Rating from '../component/Rating';
 import {parseRequestUrl} from '../utils';
 
 
@@ -8,9 +9,58 @@ const ProductScreen = {
         console.log(request);
         const product = await getProduct(request.id);
         if(product.error) {
-            return `<h1>${product.error}</h1>`;  
+            return `
+            <div>${product.error}</div>
+            `;  
         }
-        return `<h1>${product.name}</h1>`;  
+        return `
+        <div class="content">
+                <div class="back-to-result">
+                    <a href="#">Back to result</a>
+                </div>
+                <div class="details">
+                    <div class="details-image">
+                        <img src="${product.image}" alt="${product.image}" />
+                    </div>
+                    <div class="details-info">
+                        <ul>
+                            <li>
+                                <h1>${product.name}</h1>
+                            </li>
+                            <li>
+                                <h1>${Rating.render({
+                                    value: `${product.rating}`, 
+                                    text: `${product.numReviews} reviews`
+                                })}</h1>
+                            </li>
+                            <li>
+                                Price: <strong> ${product.price}</strong>
+                            </li>
+                            <li>
+                                Description:
+                                <div> 
+                                 ${product.description}
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="details-action">
+                        <ul>
+                            <li>
+                                Price: $${product.price}
+                            </li>
+                            <li>
+                                Status: ${product.countInStock > 0 ? `<span class="success">In Stock</span>` : `<span class="error">Unavailable</span>`}
+                            </li>
+                            <li>
+                                <button id="add-button" class="fw primary">
+                                Add to Cart</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        `;  
     }
 }
 
