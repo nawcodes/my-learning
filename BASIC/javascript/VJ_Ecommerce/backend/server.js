a3c2216c4edfda63630389e65eea4c427c57ee90
Nawcodes <nawcodes@gmail.com>
2021-12-14 00:07:33 +0700

    build backend signin | install expressAsyncHandler, JWTtoken, body-parse

--- BASIC/javascript/VJ_Ecommerce/backend/server.js
+++ BASIC/javascript/VJ_Ecommerce/backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import data from './data.js';
 import mongoose from 'mongoose';
 import config from './config.js';
@@ -22,6 +23,7 @@ const port = 3000;
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use('/api/users', userRouter);
 app.get("/api/products", (req, res) => {
     res.send(data.products);
@@ -42,7 +44,12 @@ app.get('/api/products/:id', (req, res) => {
         )
     }
 });
-
+app.use((err,req,res,next) => {
+    const status = err.name && err.name === 'ValidationError' ? 400: 500;
+    res.status(status).send({
+        message:err.message
+    });
+})
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
 });
