da805b0283d50c5c0c43cbb86e1c12d2a4643dab
Nawcodes <nawcodes@gmail.com>
2021-12-14 22:46:10 +0700

    build sign in action

--- BASIC/javascript/VJ_Ecommerce/frontend/src/index.js
+++ BASIC/javascript/VJ_Ecommerce/frontend/src/index.js
@@ -4,6 +4,7 @@ import Error404Screen from './srcreens/Error404Screen.js';
 import { parseRequestUrl } from './utils.js';
 import CartScreen from './srcreens/CartScreen.js';
 import SigninScreen from './srcreens/SigninScreen.js';
+import Header from './component/Header.js';
 
 
 
@@ -18,14 +19,16 @@ const routes = {
 
 const router = async () => {
     const request = parseRequestUrl();
-    const main = document.getElementById('main-container');
-
+    
     const parseUrl = 
-        (request.resource ? `/${request.resource}` : '/') +
-        (request.id ? `/:id` : '') + 
-        (request.verb ? `/${request.verb}` : '');
-
+    (request.resource ? `/${request.resource}` : '/') +
+    (request.id ? `/:id` : '') + 
+    (request.verb ? `/${request.verb}` : '');
     const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
+    const header = document.getElementById('header-container');
+    header.innerHTML = await Header.render();
+    await Header.after_render();
+    const main = document.getElementById('main-container');
     main.innerHTML = await screen.render();
     await screen.after_render();
 }
