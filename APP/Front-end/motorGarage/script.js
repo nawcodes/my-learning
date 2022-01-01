dbe2d85fc9a51039ecf8f847aba9eef29242b05c
Nawcodes <nawcodes@gmail.com>
2022-01-01 23:54:23 +0700

    MAIN LEARNING | CHANGING STRUCTURE FOLDER

--- APP/Front-end/motorGarage/script.js
+++
@@ -1,6 +0,0 @@
-const radio = document.getElementById('radio1');
-const slideFirst = document.querySelector('.first');
-
-radio.addEventListener('click', (e) => {
-    slideFirst.style.marginLeft = '-20%';
-});
