8163329cd7154b0ae77fc389c7be1cc30b681ec3
Nawcodes <nawcodes@gmail.com>
2022-01-24 15:08:41 +0700

    CRUD\EXPRESS_CRUD| Unfixed upload files

--- FRAMEWORK/js/CRUD/EXPRESS_CRUD/app.js
+++ FRAMEWORK/js/CRUD/EXPRESS_CRUD/app.js
@@ -2,6 +2,8 @@
 const express           = require('express'),
       cors              = require('cors'),
       expressLayouts    = require('express-ejs-layouts'),
+      multer = require('multer'),
+      path = require('path'),
       Data           = require('./src/model/ex_data'),
       {body, check, validationResult} = require('express-validator');
       
@@ -9,6 +11,18 @@ require('./src/utils/db');
 // declare requirement
 const port = 3000;
 const app = express();
+
+const diskStorage = multer.diskStorage({
+    destination: (req, res, cb) => {
+        cb(null, path.join(__dirname, '/uploads'));
+    }, 
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, `${file.fieldname} - ${Date.now() + path.extname(file.originalname)}`);
+    }
+})
+
+
 // use requirement after declare
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
@@ -16,6 +30,7 @@ app.use(cors());
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}))
+
 
 // routing handle 
 app.get('/', async (req, res) => {
@@ -40,42 +55,45 @@ app.post('/data',
      check('email', 'Email must be valid email').isEmail(),
      body('phone', 'Field Phone is Required & must be ID format').isMobilePhone('id-ID'),
  ],
+ multer({storage: diskStorage}).single('image'),
  (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        // res.send(errors);
-        console.log(errors);
-        res.render('page/create-form', {
-            title: 'Create form',
-            layout: 'layouts/main-layout',
-            errors : errors.array(),
-            req : req.body,
-        })
-    }else{
-        const uuid =  String(Math.floor(Math.random() * Date.now())).substr(0,11);
-        try {
+    const file = req.file.path;
+    console.log(file);
+    // const errors = validationResult(req);
+    // if(!errors.isEmpty()) {
+    //     // res.send(errors);
+    //     console.log(errors);
+    //     res.render('page/create-form', {
+    //         title: 'Create form',
+    //         layout: 'layouts/main-layout',
+    //         errors : errors.array(),
+    //         req : req.body,
+    //     })
+    // }else{
+    //     const uuid =  String(Math.floor(Math.random() * Date.now())).substr(0,11);
+    //     try {
 
-            Data.insertMany({
-            uuid: uuid,
-            name: req.body.name,
-            email: req.body.email,
-            phone: req.body.phone,
+    //         Data.insertMany({
+    //         uuid: uuid,
+    //         name: req.body.name,
+    //         email: req.body.email,
+    //         phone: req.body.phone,
             
-            }, (err, result) => {
-                if(err) {
-                    console.log(err);
-                }
-                console.log(result);
-            });
+    //         }, (err, result) => {
+    //             if(err) {
+    //                 console.log(err);
+    //             }
+    //             console.log(result);
+    //         });
 
-            return res.redirect('/');            
-        } catch (error) {
-            console.log(error);
-            res.status(500).send({message: error.message});
-        }
+    //         return res.redirect('/');            
+    //     } catch (error) {
+    //         console.log(error);
+    //         res.status(500).send({message: error.message});
+    //     }
     
-        return res.redirect('/')
-    }
+    //     return res.redirect('/')
+    // }
     
 });
 
