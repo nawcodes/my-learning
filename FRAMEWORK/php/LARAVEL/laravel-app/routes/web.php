fb15527fa3789f4cf3814f6dfdaf2fa7d26dc962
Nawcodes <nawcodes@gmail.com>
2022-03-27 21:34:22 +0700

    LARAVEL STARTED | ROUTING | ENV

--- FRAMEWORK/php/LARAVEL/laravel-app/routes/web.php
+++ FRAMEWORK/php/LARAVEL/laravel-app/routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home',
+    ['title' => 'Home', 'name' => 'Rifal', 'email' => 'rifal@gmail.com']);
+});
+
+Route::get('/about', function () {
+    return view('about');
+
+});
+Route::get('/blog', function () {
+    return view('blog');
 });
