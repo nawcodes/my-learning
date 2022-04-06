8f2f4ed13a9d89ee395910d0ff51d0aa71ab88af
Nawcodes <nawcodes@gmail.com>
2022-04-06 23:33:24 +0700

    laravel manual seeder

--- FRAMEWORK/php/LARAVEL/laravel-app/resources/views/blog.blade.php
+++ FRAMEWORK/php/LARAVEL/laravel-app/resources/views/blog.blade.php
@@ -6,8 +6,10 @@
     <h2>
         <a href="/blog/{{ $post->slug }}"> {{$post->title}}</a>
     </h2>
-    <h5>By: {{$post['author']}}</h5>
+    <p>By: <a href="#"> {{$post->user->name}} </a> In<a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }} </a></p>
     <p>{{$post->excerpt}}</p>
+    <p><a href="/blog/{{ $post->slug }}">Readmore</a></p>
+    <hr>
 </article>
 @endforeach
 
