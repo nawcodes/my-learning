e3fa7b31504fc85eb7af4daa2e2809a455046958
Nawcodes <nawcodes@gmail.com>
2022-04-26 00:32:52 +0700

    GATES | AUTHORIZATION | ELOQUENT @CAN

--- FRAMEWORK/php/LARAVEL/laravel-app/routes/web.php
+++ FRAMEWORK/php/LARAVEL/laravel-app/routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\DashboardPostController;
 
 /*
@@ -74,7 +75,8 @@ Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'check
 // CRUD routes
 Route::resource('dashboard/posts', DashboardPostController::class)->middleware('auth');
 
-
+// admin routes
+Route::resource('dashboard/categories', AdminCategoryController::class)->except('show')->middleware('is_admin');
 
 
 
