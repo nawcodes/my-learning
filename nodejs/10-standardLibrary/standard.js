a22071e9c0c245e0db9b49972f23bb046b95d769
Nawcodes <nawcodes@gmail.com>
2022-10-11 19:10:57 +0700

    learn: module / standart library

---
+++ nodejs/10-standardLibrary/standard.js
@@ -0,0 +1,8 @@
+import os from 'os';
+
+
+
+console.info(os.platform());
+console.table(os.cpus());
+
+// this program error: .js bukan file module
---
+++ nodejs/10-standardLibrary/standard.mjs
@@ -0,0 +1,6 @@
+import os from 'os';
+
+
+console.info(os.platform());
+console.table(os.cpus());
+console.info(os.homedir());
