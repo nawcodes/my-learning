0b61f2ec29d7140bde22003043a5d8a5d814abcf
Nawcodes <nawcodes@gmail.com>
2022-10-19 19:05:53 +0700

    learn: util

---
+++ nodejs/27-util/util.mjs
@@ -0,0 +1,7 @@
+// https://nodejs.org/docs/latest-v16.x/api/util.html
+
+// util adalah modul yang berisi fungsi-fungsi yang berguna untuk mempermudah dalam pengembangan aplikasi
+
+import util from 'util';
+
+console.log(util.format("Nama: %s", "Rifal"));
