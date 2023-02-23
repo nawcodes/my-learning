be49de5b2e61ef860b4e01af2b9b233583ceb7fc
nawcodes <nawcodes@gmail.com>
2023-02-23 19:23:46 +0700

    show cartItems

--- components/Order.vue
+++ components/Order.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
-    {{ cartItems }}
-  </div>
+  <v-row>
+    <v-col cols="12">
+      <h2>Order</h2>
+      <v-list>
+        <v-list-item v-for="(item, i) in cartItems" :key="i">
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-subtitle>{{ item.price }}</v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-btn icon @click="removeItem(item)">
+              <v-icon>mdi-minus</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
