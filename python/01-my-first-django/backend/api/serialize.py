e5856a56b21c3ab36161a64e27383ec1356bc6c3
Nawcodes <nawcodes@gmail.com>
2025-02-06 21:45:11 +0700

    Add Note model, serializer, views, and URLs for notes functionality

--- python/01-my-first-django/backend/api/serialize.py
+++ python/01-my-first-django/backend/api/serialize.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
+
+# this serializer is used to convert the model instances to JSON format
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +13,11 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ['id', 'title', 'content', 'created_at', 'updated_at', 'author']
+        extra_kwargs = {'author': {'read_only': True}}
+
+    
