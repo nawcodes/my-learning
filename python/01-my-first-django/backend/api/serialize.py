627756534b85739cef2997ec7997443d2b452cd1
Rifal Nurjamil <81290043+nawcodes@users.noreply.github.com>
2025-02-17 16:18:26 +0700

    Python (#24)
    * Add initial Django project structure for notes app
    
    * Update readme with Django project initialization command
    
    * Setup Django REST API with user registration and JWT authentication
    
    * Add Snake Game App with Turtle Graphics
    
    * Add Note model, serializer, views, and URLs for notes functionality
    
    * Add sample weather data CSV for states game
    
    * Setup React frontend with Vite, TypeScript, and authentication flow. for FE myAppDjanggo
    
    * Add weather data processing script and sample data files
    
    * init states game

---
+++ python/01-my-first-django/backend/api/serialize.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Note
+
+# this serializer is used to convert the model instances to JSON format
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ['id', 'title', 'content', 'created_at', 'updated_at', 'author']
+        extra_kwargs = {'author': {'read_only': True}}
+
+    
