6cc9ad57f7ee1fe30c8054229ba50148d0ccef2e
nawcodes <nawcodes@gmail.com>
2025-03-13 15:03:20 +0700

    Section 2 15-25

---
+++ flutter/first_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Text("I believe i can RICH")));
+}
+
+/**
+ * 2. Sections untitled
+ * context
+ * - How to import this fucking package
+ * - How to use the function widget like MaterialApp
+ * - then u should know if has curly named parameter, u must consistent to write that parameter. (important)
+ * output
+ * - display simple widget like text
+ * 
+ * import 'package:flutter/material.dart';
+
+  void main() {
+    runApp(MaterialApp(home: Text("I believe i can RICH")));
+  }
+ */
+
+/**
+ * 1. How flutter run himself
+ */
+// void main() {
+//   runApp();
+// }
