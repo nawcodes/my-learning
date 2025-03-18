9ed88e93d083a8bc2c074f52200b0da81f9baf96
nawcodes <nawcodes@gmail.com>
2025-03-18 16:44:00 +0700

    class & constructor?

--- flutter/first_app/lib/main.dart
+++ flutter/first_app/lib/main.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text("I believe i can RICH")));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  );
 }
 
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.blue, Colors.red],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Nawcodes",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
 /**
  * 2. Sections untitled
  * context
